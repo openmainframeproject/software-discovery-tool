6744d9948a33d587ab8ec16ffa8ee754dad6d953
Apurv Sonawane <apurvsonawane28@gmail.com>
2024-07-03 15:46:47 +0530

    added react routing
    Signed-off-by: Apurv Sonawane <apurvsonawane28@gmail.com>

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,16 +1,25 @@
-import "./app.css";
-import Footer from "./components/Footer/Footer";
-import Navbar from "./components/Navbar";
-import './index.css'; 
-import LandingPage from "./screens/LandingPage";
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import './App.css'
+
+import Navbar from './components/Navbar'
+import Footer from './components/Footer/Footer'
+import LandingPage from './screens/LandingPage'
+import Faq from './screens/Faq/Faq'
+
 function App() {
   return (
-    <>
-    {/* <Navbar/> */}
-    <LandingPage />
-    <Footer/>
-    </>
-  );
+    <Router>
+      <div className='page'>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<LandingPage />} />
+          <Route path='/faq' element={<Faq />} />
+        </Routes>
+      </div>
+      <Footer />
+    </Router>
+  )
 }
 
-export default App;
+export default App
--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
