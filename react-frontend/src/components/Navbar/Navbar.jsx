60113f15b287652d4b50d0b4a6c9ab4ab064bc5e
Deveshi Dwivedi <deveshigurgaon@gmail.com>
2024-08-20 20:56:24 +0530

    navbar and faq tweaks
    Signed-off-by: Deveshi Dwivedi <deveshigurgaon@gmail.com>

--- react-frontend/src/components/Navbar/Navbar.jsx
+++ react-frontend/src/components/Navbar/Navbar.jsx
@@ -23,8 +23,6 @@ function NavBar() {
         <nav className={`navbar-links ${isOpen ? 'active' : ''}`}>
           <Link to="/" className="navbar-link poppins-bold-black-20px hover:text-[#044FC0]">Home</Link>
           <Link to="/faq" className="navbar-link poppins-bold-black-20px hover:text-[#044FC0]">FAQ</Link>
-          <Link to="/blog" className="navbar-link poppins-bold-black-20px hover:text-[#044FC0]">Blog</Link>
-          <Link to="/contact" className="navbar-link poppins-bold-black-20px hover:text-[#044FC0]">Contact</Link>
           <a href="https://software-discovery-tool.readthedocs.io/en/latest/index.html" className="navbar-link poppins-bold-black-20px hover:text-[#044FC0]" target="_blank" rel="noopener noreferrer">Documentation</a>
         </nav>
       </div>
